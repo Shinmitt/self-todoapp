e8f2f9345193d48e371f17531f7a9c50f97ff882
shinmitt <shin0510100@gmail.com>
2023-10-16 00:52:02 +0900

    コンポーネントのインポート

---
+++ src/app/components/AddTask.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddTsk = () => {
+  return (
+    <div>AddTsk</div>
+  )
+}
+
+export default AddTsk
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import AddTsk from './components/AddTask';
+import TodoList from './components/TodoList';
 
 export default function Home() {
   return (
-   <div></div>
+   <main>
+    <h1>Nextjs 13 Todo App</h1>
+    <div>
+      <div>
+        <AddTask />
+        <TodoList />
+      </div>
+    </div>
+   </main>
   )
 }
