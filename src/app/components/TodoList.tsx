721302bbe4d0dc9584c1e93516c0d0b57b8d9f5c
shinmitt <shin0510100@gmail.com>
2023-10-17 00:15:51 +0900

    TodoListの作成

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const TodoList = () => {
   return (
-    <div>TodoList</div>
+    <ul className="space-y-3">
+        <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
+            <span>散歩</span>
+            <div>
+                <botton className="text-green-500 mr-3">edit</botton>
+                <botton className="text-red-500">Delete</botton>
+            </div>
+        </li>
+        <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
+            <span>散歩</span>
+            <div>
+                <botton className="text-green-500 mr-3">edit</botton>
+                <botton className="text-red-500">Delete</botton>
+            </div>
+        </li><li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
+            <span>散歩</span>
+            <div>
+                <botton className="text-green-500 mr-3">edit</botton>
+                <botton className="text-red-500">Delete</botton>
+            </div>
+        </li>
+    </ul>
   )
 }
 
