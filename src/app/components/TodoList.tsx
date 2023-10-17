4cc64694d0f068c5089a6641826071440ed04732
shinmitt <shin0510100@gmail.com>
2023-10-17 22:03:54 +0900

    api作成

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -1,28 +1,25 @@
-import React from 'react'
+import {Todo} from "@/types";
+import React,{useEffect}from 'react'
 
-const TodoList = () => {
+interface TodoListProps{
+    todos: Todo[];
+}
+
+const TodoList = ({todos}: TodoListProps) => {
   return (
     <ul className="space-y-3">
-        <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
-            <span>散歩</span>
-            <div>
-                <botton className="text-green-500 mr-3">edit</botton>
-                <botton className="text-red-500">Delete</botton>
-            </div>
-        </li>
-        <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
-            <span>散歩</span>
-            <div>
-                <botton className="text-green-500 mr-3">edit</botton>
-                <botton className="text-red-500">Delete</botton>
-            </div>
-        </li><li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
-            <span>散歩</span>
-            <div>
-                <botton className="text-green-500 mr-3">edit</botton>
-                <botton className="text-red-500">Delete</botton>
-            </div>
-        </li>
+        {todos.map((todo)=> (
+            <li
+            key={todo.id}
+            className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow"
+            >
+                <span>{todo.text}</span>
+                <div>
+                    <botton className="text-green-500 mr-3">edit</botton>
+                    <botton className="text-red-500">Delete</botton>
+                </div>
+            </li>
+        ))}
     </ul>
   )
 }
